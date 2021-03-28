8d2f88bc4b9e60a83876e51949753a6f066a800a
AbeLegend <fikriabu40@gmail.com>
2021-03-28 13:46:52 +0700

    Redirect to Todo Page If Still Login in Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Home = ({ history }) => {
+  // use
+  const { auth } = useSelector((state) => ({ ...state }));
+
+  React.useEffect(() => {
+    if (auth !== null) {
+      if (auth.token) {
+        history.push("/todo");
+      }
+    }
+  }, [auth, history]);
+
   return (
     <div className="flex flex-col font-primary h-screen bg-blue-primary text-white items-center justify-center">
       <h1 className="text-3xl sm:text-5xl font-bold">MERN-PWA-To Do List</h1>
