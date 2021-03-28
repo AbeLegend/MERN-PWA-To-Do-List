e0991b50738601a48e43f2c966c2fef4c5f4be49
AbeLegend <fikriabu40@gmail.com>
2021-03-28 08:56:07 +0700

    Prepare deploy API
    Client
    - Change config purge
    Server
    - Add Procfile

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = ({ history }) => {
   return (
     <div className="flex flex-col font-primary h-screen bg-blue-primary text-white items-center justify-center">
-      <h1 className="text-5xl font-bold">MERN-PWA-To Do List</h1>
+      <h1 className="text-3xl sm:text-5xl font-bold">MERN-PWA-To Do List</h1>
       <div className="flex justify-around w-screen">
         <button
           className="p-3 bg-white text-black font-semibold rounded-sm mt-10 hover:text-blue-primary"
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   purge: {
+    enabled: true,
     content: ['./src/**/*.{js,jsx}', './public/index.html']
   },
   darkMode: false, // or 'media' or 'class'
@@ -17,24 +18,13 @@ module.exports = {
       colors: {
         green: {
           primary: "#4c9a2a",
-          secondary: "#76ba1b",
-          dark: "#1e5631",
-          light: "#a4de02",
         },
         blue: {
           primary: "#006abc",
           secondary: "#2080c9",
-          dark: "#001d64",
           light: "#0098f1"
         },
-        red: {
-          primary: "#ff0000",
-          secondary: "#ff5555",
-          dark: "#aa0000",
-          light: "#ff8080"
-        }
       },
-
     },
   },
   variants: {
